bfa86a5be39e5510ff764d4dedde4a7eb00c8cd4
kathyC <575949391@qq.com>
2020-08-28 21:25:28 +0800

    feat:递归：数值的整次方

---
+++ JZ_code/16_myPow.js
@@ -0,0 +1,26 @@
+// 参考：https://xxoo521.com/2019-12-31-pow/
+// 数值的整次方
+
+// 递归
+
+// 如果exponent是偶数，Power(base, exponent) = Power(base, exponent / 2) * Power(base, exponent / 2)
+// 如果exponent是奇数，Power(base, exponent) = base * Power(base, exponent / 2) * Power(base, exponent / 2)
+
+var myPow = function (x, n) {
+  const isNegative = n < 0  // isNegative 为布尔值
+  const res = absMyPow(x, Math.abs(n))  // 需要考虑指数为负数的情况，难点
+
+  return isNegative ? 1 / res : res
+}
+
+function absMyPow (base, exponent) {
+  if (exponent === 0) return 1
+
+  else if (exponent === 1)  return base
+
+  // 难点
+  else {
+    let subRes = absMyPow(base, Math.floor(exponent / 2))
+    return exponent % 2 ? subRes * subRes * base : subRes * subRes
+  } 
+}
