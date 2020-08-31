6fbbcc657bc9faaa9af9ea871ccc6ac76118b741
kathyC <575949391@qq.com>
2020-08-31 13:28:18 +0800

    fix:链表倒数K

--- algorithm/list_k.js
+++ algorithm/list_k.js
@@ -1,15 +1,43 @@
 // 链表的倒数第k个节点
+// https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/solution/mian-shi-ti-22-lian-biao-zhong-dao-shu-di-kge-j-11/
 
 function FindKthToTail( head, k) {
-  let count = 0
-  let ret = null
+  let former = head
+  let latter = head
 
-  if(head == null) return null;
+  for (let i=0; i<k; i++) {
+    former = former.next
+  }
 
-  FindKthToTail(head.next,k);
+  while (former !== null) {
+    former = former.next
+    latter = latter.next
+  }
 
-  count ++;
-  if(count == k) ret = head;
-
-  return ret;
+  return latter
 }
+
+
+
+
+let head1 = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 2,
+          next: {
+            val: 5,
+            next: null
+          }
+        }
+      }
+    }
+  }
+}
+
+console.log(FindKthToTail(head1, 2))
