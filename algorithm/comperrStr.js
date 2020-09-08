8c191148782b07ba6b3d1f9c7c56854c74999d63
kathyC <575949391@qq.com>
2020-09-08 14:48:39 +0800

    feat:压缩字符串

---
+++ algorithm/comperrStr.js
@@ -0,0 +1,31 @@
+
+
+var comperStr = function (str) {
+  let result = ''
+  result += str[0]
+  let count = 1
+
+  for (let i=1; i<str.length; i++) {
+    if (str[i] === result[result.length-1]) {
+      count += 1
+    } else {
+      if (count > 1) {
+        result = result + '' + count
+       
+      }
+      count = 1
+      result += str[i]
+    }
+  }
+
+  if (count>1) {
+    result = result + '' + count
+  }
+
+  return result
+}
+
+
+let str = 'aabccccaaa'
+
+console.log(comperStr(str)) // a2bc4a3
