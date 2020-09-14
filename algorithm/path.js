c90554a4cb07088a90da09cc52478f017dc24e0d
kathyC <575949391@qq.com>
2020-09-14 13:05:30 +0800

    fix:矩阵路径

--- algorithm/path.js
+++ algorithm/path.js
@@ -16,6 +16,7 @@
  */
 
 // 思路：https://leetcode-cn.com/problems/unique-paths/solution/
+// 重要图示：https://leetcode-cn.com/problems/unique-paths/solution/tu-jie-bu-tong-lu-jing-shu-c-by-vin-18/
 
 /**
  * @param {number} m 行数
@@ -24,13 +25,16 @@
  */
 
 var paths = function(m, n) {
-  let dp = [m][n]
+  // 重难点：创建二维数组，值为0
+  let dp = Array.from({length:m}, () => Array.from({length:n}, () => 0))
 
+  // 注意，对于第一行 dp[0][j]，或者第一列 dp[i][0]，由于都是在边界，所以只能为 1
   for (let i=0; i<n; i++) dp[0][i] = 1  // 将第一行初始为1
   for (let j=0; j<m; j++) dp[j][0] = 1  // 将第一列初始为1
 
   for (let i=1; i<m; i++) { // 从第二行第二列开始遍历，记录到达每一个格子的不同路径数
     for (let j=1; j<n; j++) {
+      // 右下格子：右 -> 下 || 下 -> 右
       dp[i][j] = dp[i-1][j] + dp[i][j-1]  // 相邻并且已遍历格子 的路径数之和
     }
   }
@@ -38,7 +42,7 @@ var paths = function(m, n) {
   return dp[m-1][n-1]
 };
 
-console.log(paths(3, 6))
+console.log(paths(3, 7))  // 28
 
 
 
