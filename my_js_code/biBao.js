d0adeadce16faace93d79036565b92a90b330743
kathyC <575949391@qq.com>
2020-11-05 01:04:50 +0800

    feat:手写内存泄漏例子

---
+++ my_js_code/biBao.js
@@ -0,0 +1,11 @@
+function outFn() {
+  var num = 123    // 被闭包所引用的变量，不会被回收
+  
+	return function() {
+		console.log(num)
+	}
+}
+// 调用完了函数以后,变量仍然存在,导致泄漏
+
+// 查看内存情况：
+// 命令行可以使用 Node 提供的process.memoryUsage方法
