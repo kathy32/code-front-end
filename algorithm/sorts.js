20948eecb4a4f2f23202096d56adf7c160b72a06
kathyC <575949391@qq.com>
2020-11-18 12:27:25 +0800

    feat:排序年龄

--- algorithm/sorts.js
+++ algorithm/sorts.js
@@ -23,3 +23,34 @@ let quickSort = function (arr) {
 // console.log(quickSort ([2,3,4,5,1]))
 
 
+// 应用 - 腾讯云一面（按照年龄排序）
+let par = [{age:23, name: 'yico'}, 
+            {age: 7, name: 'neymar'}, 
+            {age: 18, name: 'kathy'}]
+
+
+var sortAge = function (par) {
+  if (par.length <= 1) return par 
+
+  let pivotIndex = Math.floor(par.length / 2)
+  let pivot = par.splice(pivotIndex, 1)[0].age
+  let tempPivot = par.splice(pivotIndex, 1)[0]
+  let right = []
+  let left = []
+
+  for (let i=0; i<par.length; i++) {
+    if (pivot > par[i].age) {
+      left.push(par[i])
+      
+    } else {
+      right.push(par[i])
+    }
+  }
+
+  return sortAge(left).concat(tempPivot, sortAge(right))
+
+}
+
+console.log(sortAge(par))
+
+
